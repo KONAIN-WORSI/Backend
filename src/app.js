f828210d05a4e53597ea5fd8f40b447d2603b8b7
KONAIN-WORSI <konainworsi@gmail.com>
2026-08-24 17:50:39 +0545

    added the user registration controller with all the bug fixed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import cookieParser from "cookies-parser"
+import cookieParser from "cookie-parser";
 
 const app = express()
 
@@ -17,9 +17,13 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser());
 
+// routes import
+import userRouter from './routes/user.routes.js'
 
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 
 
